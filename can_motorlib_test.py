d017def9ab376560f0481df867fb0385aca07f5f
Shubham Vyas <Shubham.Vyas@dfki.de>
2021-05-04 17:47:04 +0200

    update with minor changes

--- can_motorlib_test.py
+++ can_motorlib_test.py
@@ -1,5 +1,5 @@
 import sys
-# import time
+import time
 import numpy as np
 from canmotorlib import CanMotorController
 
@@ -15,20 +15,25 @@ print("Using Socket {} for can communication".format(sys.argv[1],))
 
 motor_controller = CanMotorController(sys.argv[1], motor_id)
 
-pos, vel, curr = motor_controller.enable_motor()
+startTime = time.perf_counter()
 
-# time.sleep(1)
+pos, vel, curr = motor_controller.enable_motor()
 
 # pos, vel, curr = motor_controller.send_deg_command(0, 0, 0, 0, 0)
 print("Initial Position: {}, Velocity: {}, Torque: {}".format(np.rad2deg(pos), np.rad2deg(vel),
                                                                 curr))
 
+endTime = time.perf_counter()
+
+print("Time for One Command: {}".format(endTime - startTime))
+
+time.sleep(1)
 
 while abs(np.rad2deg(pos)) > 0.5:
     pos, vel, curr = motor_controller.set_zero_position()
     print("Position: {}, Velocity: {}, Torque: {}".format(np.rad2deg(pos), np.rad2deg(vel), curr))
 
-# time.sleep(1)
+time.sleep(1)
 
 pos, vel, curr = motor_controller.disable_motor()
 print("Final Position: {}, Velocity: {}, Torque: {}".format(np.rad2deg(pos), np.rad2deg(vel), curr))
